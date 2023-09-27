7bac1a428a7adb5714c0e5a73263e853a353affa
cantikaposumah <145555392+cantikaposumah@users.noreply.github.com>
2023-09-27 16:07:55 +0700

    Add files via upload

---
+++ main111.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	//Deklarasi
+	int p=90;
+	int l=80;
+	int t=70;
+	
+	//Input dari user
+	cout<<"Masukan p=90";
+	cin>>p=90;
+	cout<<"Masukan l=80";
+	cin>>l=80;
+	cout<<"Masukan t=70";
+	cin>>t=70;
+	
+	//Process
+	t = p x l;
+	
+	//output
+	cout<<"Hasil "<< t;
+	
+	return 0
+}
